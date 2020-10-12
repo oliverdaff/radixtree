ab4d6c269fe42ab1adb0ab00396105809de268d4
Oliver Daff <oliverdaff@users.noreply.github.com>
2020-10-12 16:07:48 +1000

    added longest common prefix test

--- radixtree_test.go
+++ radixtree_test.go
@@ -1,1 +1,23 @@
 package radixtree
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLongestCommonPrefix(t *testing.T) {
+	var tests = []struct {
+		first, second, prefix string
+	}{
+		{"abc", "ade", "a"},
+	}
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%s:%s", tt.first, tt.second)
+		t.Run(testname, func(t *testing.T) {
+			prefix := longestCommonPrefix(tt.first, tt.second)
+			if prefix != tt.prefix {
+				t.Errorf("Expected prefix %s got %s", tt.prefix, prefix)
+			}
+		})
+	}
+}
