f5a38976b3a2ecb747bfedf7f365e2d97a6e383b
Oliver Daff <oliverdaff@users.noreply.github.com>
2020-10-12 16:18:24 +1000

    added newRadixTreeNode test

--- radixtree_test.go
+++ radixtree_test.go
@@ -5,6 +5,28 @@ import (
 	"testing"
 )
 
+func TestNewRadixTreeNode(t *testing.T) {
+	var tests = []struct {
+		key   string
+		value interface{}
+	}{
+		{"abc", 1},
+	}
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%s:%s", tt.key, tt.value)
+		t.Run(testname, func(t *testing.T) {
+			node := newRadixTreeNode(tt.key, tt.value)
+			if node.size != 1 {
+				t.Errorf("Expected size of 1 got %d", node.size)
+			}
+			if node.linksByFirstChar[tt.key[0]] != tt.key {
+				t.Errorf("Expected links by first char %s to be %s", string(tt.key[0]), tt.key)
+			}
+		})
+
+	}
+}
+
 func TestLongestCommonPrefix(t *testing.T) {
 	var tests = []struct {
 		first, second, prefix string
